8ed878b61258740a324f22711d80803c7c686a42
Damir Drempetić <damir.drempetic@live.com>
2023-03-24 14:16:58 +0100

    Create rollup.config.mjs

---
+++ rollup.config.mjs
@@ -0,0 +1,34 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+import packageJson from "./package.json" assert { type: "json" };
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "bin/esm/types/index.d.ts",
+    output: [{ file: "bin/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
